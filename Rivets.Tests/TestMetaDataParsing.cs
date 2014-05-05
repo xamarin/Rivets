fa8da8e10569f4af3d9acf52cb40415344731860
Redth <jondick@gmail.com>
2014-05-05 16:26:53 -0400

    Added Tests Project

---
+++ Rivets.Tests/TestMetaDataParsing.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+
+namespace Rivets.Tests
+{
+	[TestFixture ()]
+	public class TestMetaDataParsing
+	{
+
+		[SetUp]
+		public void Setup()
+		{
+			var url = "http://localhost:4477";
+			var root = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "..", "..", "Html");
+
+			var fileSystem = new PhysicalFileSystem(root);
+
+			var options = new FileServerOptions
+			{
+				EnableDirectoryBrowsing = true, 
+				FileSystem = fileSystem                             
+			};
+
+			WebApp.Start(url, builder => builder.UseFileServer(options));            
+			Console.WriteLine("Listening at " + url);
+
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+
+		}
+
+		[Test ()]
+		public void SimpleAndroidMetaDataTest ()
+		{
+			var resolver = new HttpClientAppLinkResolver ();
+			var appLinks = resolver.ResolveAppLinks ("http://localhost:4477/SimpleAndroidMetaData.html").Result;
+
+			Assert.IsNotNull (appLinks);
+			Assert.Greater (appLinks.Targets.Count, 0);
+		}
+	}
+}
+
