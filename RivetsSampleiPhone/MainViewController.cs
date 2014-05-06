3fd107699977d2c91911ac8f5ace4b5a9c72ad97
Redth <jondick@gmail.com>
2014-05-06 09:20:14 -0400

    Added iOS iPhone Sample app

---
+++ RivetsSampleiPhone/MainViewController.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+
+namespace RivetsSampleiPhone
+{
+	public partial class MainViewController : DialogViewController
+	{
+		ProductViewController productViewController;
+
+		public MainViewController () : base (UITableViewStyle.Grouped, null)
+		{
+			Root = new RootElement ("Rivets Sample") {
+				new Section () {
+					new StyledStringElement("Show Product: widget", () => {
+						productViewController = new ProductViewController("widget");
+						NavigationController.PushViewController(productViewController, true);
+					}),
+					new StyledStringElement("App Link to widget", async () => {
+						var url = "https://rawgit.com/Redth/Rivets/master/Rivets.Tests/Html/SimpleiOSMetaData.html";
+
+						var resolver = new Rivets.HttpClientAppLinkResolver();
+						var appLinks = await resolver.ResolveAppLinks(url);
+
+						var navigator = new Rivets.AppLinkNavigator();
+						var result = await navigator.Navigate(appLinks, null);
+
+						Console.WriteLine(result);
+					})
+				},
+			};
+		}
+	}
+}
---
+++ RivetsSampleiPhone/ProductViewController.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+using System.Drawing;
+
+namespace RivetsSampleiPhone
+{
+	public partial class ProductViewController : DialogViewController
+	{
+		public ProductViewController (string productId) : base (UITableViewStyle.Grouped, null, true)
+		{
+			ProductId = productId;
+			Initialize ();
+		}
+
+		public ProductViewController (string productId, Rivets.AppLink refererAppLink) : base (UITableViewStyle.Grouped, null, refererAppLink == null)
+		{
+			ProductId = productId;
+			RefererAppLink = refererAppLink;
+			Initialize ();
+		}
+
+	
+		void Initialize()
+		{
+			Root = new RootElement ("View Product") {
+				new Section () {
+					new StyledStringElement("Product ID", ProductId)
+				},
+			};
+		}
+
+		public string ProductId { get; private set; }
+		public Rivets.AppLink RefererAppLink { get; private set; }
+
+		Rivets.ReturnToRefererViewController returnToRefererController;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			returnToRefererController = new Rivets.ReturnToRefererViewController ();
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+
+			if (RefererAppLink != null)
+				returnToRefererController.ShowViewForRefererAppLink (RefererAppLink);
+		}
+	}
+}
