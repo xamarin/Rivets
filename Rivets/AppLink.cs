f36eb9175371936eed9abe4c80a78fd5385542be
Redth <jondick@gmail.com>
2014-05-06 17:56:57 -0400

    XML Documentation files now generated

--- Rivets/AppLink.cs
+++ Rivets/AppLink.cs
@@ -5,8 +5,22 @@ namespace Rivets
 {
 	public class AppLink
 	{
+		/// <summary>
+		/// Url that the App Link originates from
+		/// </summary>
+		/// <value>The source URL.</value>
 		public Uri SourceUrl { get; set; }
+
+		/// <summary>
+		/// Web Url that the App Link should fall back to
+		/// </summary>
+		/// <value>The web URL.</value>
 		public Uri WebUrl { get; set; }
+
+		/// <summary>
+		/// Mobile app Targets for the given Source Url
+		/// </summary>
+		/// <value>The targets.</value>
 		public List<IAppLinkTarget> Targets { get; set; }
 	}	
 }
