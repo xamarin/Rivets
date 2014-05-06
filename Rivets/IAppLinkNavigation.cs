da79020b6fff7f04be7a9cea5eedf2f962488180
Redth <jondick@gmail.com>
2014-05-06 14:16:41 -0400

    Forgotten changes from previous commit

--- Rivets/IAppLinkNavigation.cs
+++ Rivets/IAppLinkNavigation.cs
@@ -5,6 +5,19 @@ namespace Rivets
 {
 	public interface IAppLinkNavigation
 	{
+
+		Task<NavigationResult> Navigate(AppLink appLink);
+
+		Task<NavigationResult> Navigate(Uri url);
+
+		Task<NavigationResult> Navigate(string url);
+
+		Task<NavigationResult> Navigate(AppLink appLink, RefererAppLink refererAppLink);
+
+		Task<NavigationResult> Navigate(Uri url, RefererAppLink refererAppLink);
+
+		Task<NavigationResult> Navigate(string url, RefererAppLink refererAppLink);
+
 		Task<NavigationResult> Navigate(AppLink appLink, AppLinkData appLinkData);
 
 		Task<NavigationResult> Navigate(Uri url, AppLinkData appLinkData);
