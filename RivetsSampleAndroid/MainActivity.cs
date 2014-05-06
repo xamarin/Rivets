ffe80c092238d59ee1631723e62b7f0c219eff15
Redth <jondick@gmail.com>
2014-05-06 15:14:15 -0400

    Very simple Android sample, but works!

---
+++ RivetsSampleAndroid/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace RivetsSampleAndroid
+{
+	[Activity (Label = "Rivets Sample", MainLauncher = true)]
+	public class MainActivity : ListActivity
+	{
+		MainListAdapter adapter;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			//SetContentView (Resource.Layout.Main);
+
+			adapter = new MainListAdapter {
+				Context = this
+			};
+
+			ListAdapter = adapter;
+
+			ListView.ItemClick += async (sender, e) => {
+				var item = adapter[e.Position];
+
+				if (item == "Product Details: widget") {
+
+					var intent = new Intent(this, typeof(ProductActivity));
+					intent.PutExtra("PRODUCT_ID", "widget");
+					StartActivity(intent);
+
+				} else {
+
+					var url = "https://rawgit.com/Redth/Rivets/master/Rivets.Tests/Html/SimpleAndroidMetaData.html";
+					var result = await Rivets.AppLinks.Navigator.Navigate(url);
+
+					Console.WriteLine(result);
+				}
+			};
+		}
+	}
+
+
+	public class MainListAdapter : BaseAdapter<string>
+	{
+		public Activity Context { get;set; }
+
+		List<string> items = new List<string> {
+			"Product Details: widget",
+			"App Link to widget",
+		};
+
+		public override long GetItemId (int position) { return position; }
+		public override int Count { get { return items.Count; } }
+		public override string this [int index] { get { return items[index]; } }
+
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			var item = items [position];
+
+			var view = convertView ?? Context.LayoutInflater.Inflate (Android.Resource.Layout.SimpleListItem1, null);
+
+			var text1 = view.FindViewById<TextView> (Android.Resource.Id.Text1);
+			text1.Text = item;
+
+			return view;
+		}
+	}
+}
+
+
---
+++ RivetsSampleAndroid/ProductActivity.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace RivetsSampleAndroid
+{
+	[Activity (Label = "ProductActivity")]			
+	[IntentFilter(new [] {Android.Content.Intent.ActionView }, DataScheme="example", DataHost="*", Categories=new [] { Android.Content.Intent.CategoryDefault })]
+	public class ProductActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			var id = string.Empty;
+
+			if (Intent.HasExtra ("PRODUCT_ID")) {
+				id = Intent.GetStringExtra ("PRODUCT_ID");
+			} else {
+				var alUrl = new Rivets.AppLinkUrl (Intent.Data.ToString ());
+
+				if (alUrl != null && alUrl.InputQueryParameters.ContainsKey ("id")) {
+					id = alUrl.InputQueryParameters ["id"];
+				}
+			}
+
+			Toast.MakeText (this, "Display Product Id: " + id, ToastLength.Short).Show ();
+		}
+	}
+}
+
