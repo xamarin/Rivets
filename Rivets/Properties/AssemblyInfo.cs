8d0e30b2d6864218b141f3f52bebc42f196eb4e3
Redth <jondick@gmail.com>
2014-05-07 10:33:28 -0400

    Cleaned up Solution, moved things into solution folders

--- Rivets/Properties/AssemblyInfo.cs
+++ Rivets/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("Rivets")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("A C# implementation of App Links, functionally, a port of Bolts")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("redth")]
+[assembly: AssemblyCopyright ("Jonathan Dick")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("1.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
